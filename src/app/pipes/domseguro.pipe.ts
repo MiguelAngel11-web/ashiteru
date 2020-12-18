eaed05a0579ed12e36c2a920a2155d413b5d560f
MiguelAngel11-web <miguel_asc11@hotmail.com>
2020-12-17 21:52:31 -0600

    Se usa para la urls y sean seguras

--- src/app/pipes/domseguro.pipe.ts
+++ src/app/pipes/domseguro.pipe.ts
@@ -8,13 +8,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DomseguroPipe implements PipeTransform {
 
   transform(value: string, regexValue: string, replaceValue: string): any {
-/*       console.log("Value: "+ value + " RegexValue:" + regexValue+ " ReplaceValues: " +replaceValue); */
       let regex = new RegExp(regexValue,'g');
-      /* let valorreplacazdo = value.replace(regex, replaceValue);
-      console.log("VALOR: "+ valorreplacazdo ); *//*
-      console.log("VRegex: "+ regex + " ReplaceValues: " +replaceValue); */
       return value.replace(regex, replaceValue);
-/*     return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value ); */
   }
 
 }
